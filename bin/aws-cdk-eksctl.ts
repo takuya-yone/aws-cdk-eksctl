63a689e271071d0e383cf1b9299265a6e4b88b71
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2023-06-16 23:37:14 +0900

    up

--- bin/aws-cdk-eksctl.ts
+++ bin/aws-cdk-eksctl.ts
@@ -3,6 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EksVpcStack } from '../lib/eks-vpc-stack';
 import { EksClusterStack } from '../lib/eks-cluster-stack';
+import { Aspects } from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag'
+
 
 const app = new cdk.App();
 
@@ -12,6 +15,9 @@ const envTokyo = { region: 'ap-northeast-1' };
 const VpcStack = new EksVpcStack(app, 'Sandbox-EKS-VPC-Stack', {
   env: envTokyo,
 });
+
+Aspects.of(app).add(new AwsSolutionsChecks());
+cdk.Tags.of(app).add("Project", "CDKEksCtl");
 
 // const ClusterStack = new EksClusterStack(
 //   app,
